51a6888470a32046b0b9ae7ed9b36d4a4975915d
Wharton Aldrick <whartonaldrick@gmail.com>
2023-10-06 00:56:53 +0300

    animation hover

--- assets/scripts/libs/swiper/swiper-additional.js
+++ assets/scripts/libs/swiper/swiper-additional.js
@@ -2,6 +2,7 @@ var swiper = new Swiper(".animationSlide", {
     slidesPerView: 2,
     spaceBetween: 1,
     loop: true,
+    grabCursor: true,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
@@ -9,6 +10,7 @@ var swiper = new Swiper(".animationSlide", {
     },
     navigation: {
         nextEl: ".swiper-button-next",
+        //j'ai preferé l'activé
         prevEl: ".swiper-button-prev",
     },
 });
